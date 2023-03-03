9c0f313253482f2283eec58911ae368c84a31c05
danieli-santos <dannysantos1106@gmailcom>
2023-03-03 15:46:20 -0300

    cap 11

--- src/main/java/pages/LargeAndDeepDomPage.java
+++ src/main/java/pages/LargeAndDeepDomPage.java
@@ -1,2 +1,24 @@
-package pages;public class LargeAndDeepDomPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LargeAndDeepDomPage {
+
+    private WebDriver driver;
+    private By table = By.id("large-table");
+
+    public LargeAndDeepDomPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void scrollToTable(){
+        WebElement tableElement = driver.findElement(table);
+        String script = "arguments[0].scrollIntoView();";
+        ((JavascriptExecutor)driver).executeScript(script, tableElement); //Nós lançamos isso, agora é um JavascriptExecutorobjeto.
+       
+        
+    }
 }
